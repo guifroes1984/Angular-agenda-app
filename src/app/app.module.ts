6790f42a17a3aa8da72dfbca7fa1d5170fd3ebfa
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-09 15:23:40 -0300

    Criando formulário de contatos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MatButtonModule }  from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,8 @@ import { ContatoService } from './contato.service'
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
-
+    MatToolbarModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
   providers: [
